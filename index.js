6e79062e86dc3fd0dc2d759710389fa1787c150f
Jay2020-malo <j.karithi1988@gmail.com>
2023-01-09 23:39:23 +0400

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hello: 'There!! khalid'});
+    res.send({ hello: 'There!! khalid my son'});
 });
 
 const PORT = process.env.PORT || 5000;
