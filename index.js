607395a17e1dfc0d9a728e235283c89b7195cbfb
MaloJay <jaymalojay@gmail.com>
2023-01-18 21:46:27 +0400

    first changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hello: 'There!! khalid my person'});
+    res.send({ hello: 'There!! node app is awesome!!!'});
 });
 
 const PORT = process.env.PORT || 5000;
